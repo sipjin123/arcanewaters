05a45d4e3c5d14852304d3d28e6ebae1ee2339e4
B0ntaBur <burlin_gauna@yahoo.com>
2019-07-16 13:47:05 +0800

    updated combination data logic

--- Arcane Waters - Alpha/Assets/Scripts/BurlDevTestScripts/CombinationData.cs
+++ Arcane Waters - Alpha/Assets/Scripts/BurlDevTestScripts/CombinationData.cs
@@ -17,10 +17,19 @@ public class CombinationData : ScriptableObject {
         {
             DebugCustom.Print("This item needs : " + (CraftingIngredients.Type) combinationRequirements[i].itemTypeId);
         }
-        for(int i = 0; i  < itemList.Count; i++)
+
+        for(int i = 0; i < combinationRequirements.Count; i ++)
         {
-            if(combinationRequirements.Find(_=>_.itemTypeId == itemList[i].itemTypeId) != null)
+           Debug.LogError("This sword requirement is : "+ (CraftingIngredients.Type)combinationRequirements[i].itemTypeId);
+        }
+
+        for(int i = 0; i  < combinationRequirements.Count; i++)
+        {
+            Debug.LogError("Comparing : " + (CraftingIngredients.Type)itemList[i].itemTypeId);
+            //if(combinationRequirements.Find(_=> (CraftingIngredients.Type)_.itemTypeId == (CraftingIngredients.Type)itemList[i].itemTypeId) != null)
+            if (itemList.Find(_ => (CraftingIngredients.Type)_.itemTypeId == (CraftingIngredients.Type)combinationRequirements[i].itemTypeId) != null)
             {
+
             }
             else
             {
