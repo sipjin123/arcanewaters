e20d0938c1e4a56a42460c7bd0953d22351fe6df
B0ntaBur <burlin_gauna@yahoo.com>
2019-07-16 14:27:13 +0800

    added debug commands

--- Arcane Waters - Alpha/Assets/Scripts/BurlDevTestScripts/DebugButtons.cs
+++ Arcane Waters - Alpha/Assets/Scripts/BurlDevTestScripts/DebugButtons.cs
@@ -17,6 +17,37 @@ public class DebugButtons : MonoBehaviour {
             Debug.LogError("Battle stat");
             Global.player.rpc.Cmd_StartNewBattle(1);
         }
+        if (Input.GetKey(KeyCode.M))
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                CraftingIngredients craftingIngredients = new CraftingIngredients(0, (int)CraftingIngredients.Type.Ore, ColorType.DarkGreen, ColorType.DarkPurple, "");
+                craftingIngredients.itemTypeId = (int)craftingIngredients.type;
+                Item item = craftingIngredients;
+
+                PanelManager.self.rewardScreen.Show(item);
+                Global.player.rpc.Cmd_DirectAddItem(item);
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                CraftingIngredients craftingIngredients = new CraftingIngredients(0, (int)CraftingIngredients.Type.Lumber, ColorType.DarkGreen, ColorType.DarkPurple, "");
+                craftingIngredients.itemTypeId = (int)craftingIngredients.type;
+                Item item = craftingIngredients;
+
+                PanelManager.self.rewardScreen.Show(item);
+                Global.player.rpc.Cmd_DirectAddItem(item);
+            }
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                CraftingIngredients craftingIngredients = new CraftingIngredients(0, (int)CraftingIngredients.Type.Lizard_Claw, ColorType.DarkGreen, ColorType.DarkPurple, "");
+                craftingIngredients.itemTypeId = (int)craftingIngredients.type;
+                Item item = craftingIngredients;
+
+                PanelManager.self.rewardScreen.Show(item);
+                Global.player.rpc.Cmd_DirectAddItem(item);
+            }
+        }
+
     }
     #endregion
 }
