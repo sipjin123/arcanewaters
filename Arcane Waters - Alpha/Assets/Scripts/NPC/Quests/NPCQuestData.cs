bce670c86144898b04d7e271a74e671eab12c53d
B0ntaBur <burlin_gauna@yahoo.com>
2019-07-17 20:33:59 +0800

    updated class structure for quest data added npc dialogue filtering state renived redundant functions

--- Arcane Waters - Alpha/Assets/Scripts/NPC/Quests/NPCQuestData.cs
+++ Arcane Waters - Alpha/Assets/Scripts/NPC/Quests/NPCQuestData.cs
@@ -1,20 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCQuestData : ScriptableObject
 {
    #region Public Variables
    public QuestType questType;
-   public ClickableText.Type UnlockableDialogue;
 
-   public List<DeliverQuest> deliveryQuests;
+   public List<DeliveryQuestPair> deliveryQuestList;
+   public List<HuntQuestPair> huntQuestList;
+
+   public string initQuestMessage;
+   public string questCompletedMessage;
+   public ClickableText.Type initQuestAnswer;
    #endregion
 }
 
-public interface IQuest
+[Serializable]
+public class DeliveryQuestPair : QuestInfo
 {
+   public DeliverQuest DeliveryQuest;
+}
+[Serializable]
+public class HuntQuestPair : QuestInfo
+{
+   public HuntQuest HuntyQuest;
 }
 
+public class QuestInfo
+{
+   public QuestState questState;
+   public ClickableText.Type introAnswer;
+   public ClickableText.Type successAnswer;
+   public ClickableText.Type failAnswer;
+   public string unlockableDialogue;
+   public string introDialogue;
+}
+
+public enum QuestState
+{
+   None,
+   Initialized,
+   Pending,
+   Completed
+}
 public enum QuestType
 {
    Hunt,
