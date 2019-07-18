56315b85a81f4d8260d48c73937dab338fa22c0f
B0ntaBur <burlin_gauna@yahoo.com>
2019-07-18 13:50:02 +0800

    added comments removed redundancy fixed capitalizations

--- Arcane Waters - Alpha/Assets/Scripts/NPC/NPCFriendshipManager.cs
+++ Arcane Waters - Alpha/Assets/Scripts/NPC/NPCFriendshipManager.cs
@@ -3,25 +3,21 @@
 public class NPCFriendshipManager : MonoBehaviour
 {
    #region Public Variables
+
+   // Self reference
    public static NPCFriendshipManager self;
+
    #endregion
+
    private void Awake () {
       self = this;
    }
 
-   public float LoadRelationship (string npcName) {
-      return PlayerPrefs.GetFloat("NPC_NAME_" + npcName);
+   public float loadRelationship (float npcID) {
+      return PlayerPrefs.GetInt("NPC_ID_" + npcID);
    }
 
-   public float LoadRelationship (float npcID) {
-      return PlayerPrefs.GetFloat("NPC_ID_" + npcID);
-   }
-
-   public void UpdateNPCRelationship (string npcID, float amount) {
-      PlayerPrefs.SetFloat("NPC_NAME_" + npcID, amount);
-   }
-
-   public void UpdateNPCRelationship (float npcID, float amount) {
+   public void updateNPCRelationship (int npcID, int amount) {
       PlayerPrefs.SetFloat("NPC_ID_" + npcID, amount);
    }
 }
--- Arcane Waters - Alpha/Assets/Scripts/NPC/QuestManager.cs
+++ Arcane Waters - Alpha/Assets/Scripts/NPC/QuestManager.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 public class QuestManager : MonoBehaviour
 {
    #region Public Variables
+
+   // Self Reference
    public static QuestManager self;
+
+   // UI Template to instantiate
    public GameObject questTemplate;
+
+   // Quest List Holder
    public Transform questParent;
+
+   // List of current active quests
    public List<NPCQuestData> currentQuestList = new List<NPCQuestData>();
+
+   // List of current quest templates instantiated
    public List<GameObject> questTemplateList = new List<GameObject>();
+
    #endregion
 
    private void Awake () {
       self = this;
    }
 
-   public void RegisterQuest (NPCQuestData quest) {
-      GameObject temp = Instantiate(questTemplate, questParent);
-      temp.GetComponent<QuestTemplate>().questData = quest;
+   public void registerQuest (NPCQuestData quest) {
+      GameObject questUITemplate = Instantiate(questTemplate, questParent);
+      questUITemplate.GetComponent<QuestTemplate>().questData = quest;
 
-      temp.SetActive(true);
+      questUITemplate.SetActive(true);
       currentQuestList.Add(quest);
-      questTemplateList.Add(temp);
+      questTemplateList.Add(questUITemplate);
    }
 
-   public void ClearQuest (NPCQuestData data) {
-      var gameObj = questTemplateList.Find(_ => _.GetComponent<QuestTemplate>().questData == data);
-      questTemplateList.Remove(gameObj);
-      Destroy(gameObj);
+   public void clearQuest (NPCQuestData data) {
+      GameObject questUITemplate = questTemplateList.Find(_ => _.GetComponent<QuestTemplate>().questData == data);
+      questTemplateList.Remove(questUITemplate);
+      Destroy(questUITemplate);
 
       currentQuestList.Remove(data);
    }
