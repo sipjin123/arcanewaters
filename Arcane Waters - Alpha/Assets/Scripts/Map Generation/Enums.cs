331a2a314375eb3b434ecd595e8ad707c2c9835e
B0ntaBur <burlin_gauna@yahoo.com>
2019-07-24 12:49:44 +0800

    map gen and version update

---
+++ Arcane Waters - Alpha/Assets/Scripts/Map Generation/Enums.cs
@@ -0,0 +1,28 @@
+﻿namespace ProceduralMap
+{
+    public enum BorderType
+    {
+        //Four directions
+        allDirections,
+
+        //Three directions
+        topLateral,
+        downLateral,
+        leftTopDown,
+        rightTopDown,
+
+        //Two directions
+        topDown,
+        Lateral,
+        topLeft,
+        downLeft,
+        topRight,
+        downRight,
+
+        //One direction
+        top,
+        down,
+        right,
+        left
+    }
+}
---
+++ Arcane Waters - Alpha/Assets/Scripts/Map Generation/Structs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace ProceduralMap
+{
+    [System.Serializable]
+    public struct LayerType
+    {
+        public string name;
+        [Range(0, 1)]
+        public float height;
+        public bool useCollider;
+        public Tile tile;
+
+        [Header("Borders")]
+        public bool useBorderOnDiferentLayer;
+        public string BorderLayerName;
+        public Border[] borders;
+    }
+
+    [System.Serializable]
+    public struct ObjectLayer
+    {
+        public string name;
+        public string layerToPlace;
+        public Tile tile;
+    }
+
+    [System.Serializable]
+    public struct Border
+    {
+        public BorderType borderType;
+        public Tile BorderTile;
+    }
+}
