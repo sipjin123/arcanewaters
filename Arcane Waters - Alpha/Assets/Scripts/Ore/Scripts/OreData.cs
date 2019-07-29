5b8f1b230891309a778deb9b60c70f4302eb976c
B0ntaBur <burlin_gauna@yahoo.com>
2019-07-30 01:28:56 +0800

    added comments

--- Arcane Waters - Alpha/Assets/Scripts/Ore/Scripts/OreData.cs
+++ Arcane Waters - Alpha/Assets/Scripts/Ore/Scripts/OreData.cs
@@ -8,8 +8,13 @@ public class OreData : ScriptableObject {
 
    #region Public Variables
 
+   // Holds the sprite icons which changes each time the ore is mined
    public List<Sprite> miningDurabilityIcon;
+
+   // Determines the type of ore
    public OreType oreType;
+
+   // Determines the reward received after mining
    public CraftingIngredients.Type ingredientReward;
 
    #endregion
