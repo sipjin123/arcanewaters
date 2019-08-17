68ecb9769ca98301b556e9f3475d228be685eec9
B0ntaBur <burlin_gauna@yahoo.com>
2019-08-17 09:13:17 +0800

    added sea monster entity as inheritance

---
+++ Arcane Waters - Alpha/Assets/Scripts/Entities/SeaMonsterEntity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+
+public class SeaMonsterEntity : SeaEntity
+{
+   #region Public Variables
+
+   // The total tentacles left before this unit dies
+   [SyncVar]
+   public int tentaclesLeft;
+
+   // Animator
+   public Animator animator;
+
+   // Tentacle Animation
+   public enum TentacleAnimType
+   {
+      Idle,
+      Attack,
+      Die,
+   }
+
+
+   #endregion
+
+   #region Private Variables
+
+   #endregion
+}
