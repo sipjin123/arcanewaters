fec5ff8617b70b607ebff9c1060d0996e55e72ac
B0ntaBur <burlin_gauna@yahoo.com>
2019-12-05 07:49:47 +0800

    initializes xml managers text assets

---
+++ Arcane Waters - Alpha/Assets/Project Tools/Util/TextAssetInitializer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+
+public class TextAssetInitializer : MonoBehaviour {
+   #region Public Variables
+
+   #endregion
+
+   private void Awake () {
+      XmlManager[] xmlManagers = GetComponentsInChildren<XmlManager>();
+      foreach (XmlManager manager in xmlManagers) {
+         manager.loadAllXMLData();
+      }
+   }
+
+   #region Private Variables
+
+   #endregion
+}
