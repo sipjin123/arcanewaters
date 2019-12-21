73086a2ca1d2b0bca500607800c1effb133d768a
Dev_Burlin <burlin_gauna@yahoo.com>
2019-12-21 15:50:30 +0800

    tool scene updates / data updates / util setup for editor tools / prefab update for projectile

---
+++ Arcane Waters - Alpha/Assets/Project Tools/Util/XmlLoadingPanel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+
+public class XmlLoadingPanel : MonoBehaviour {
+   #region Public Variables
+
+   // Self reference
+   public static XmlLoadingPanel self;
+
+   // The canvas that will block the view if it is loading
+   public Canvas loadBlocker;
+
+   #endregion
+
+   private void Awake () {
+      self = this;
+   }
+
+   public void startLoading () {
+      loadBlocker.enabled = true;
+   }
+
+   public void finishLoading() {
+      loadBlocker.enabled = false;
+   }
+
+   #region Private Variables
+
+   #endregion
+}
