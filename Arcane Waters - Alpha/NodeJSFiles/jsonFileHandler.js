bbcd56d28293245ca932ff0cfb957f6236e87bc4
Dev_Burlin <burlin_gauna@yahoo.com>
2021-02-05 19:58:12 +0800

    created new class to be passed back to web script updated query to trigger another function which will then fetch the plastic comments right after fetching deploy history added logs so in-case things go wrong, the cmd can be reviewed in progression setup caching and data assignment after fetching data from database

--- Arcane Waters - Alpha/NodeJSFiles/jsonFileHandler.js
+++ Arcane Waters - Alpha/NodeJSFiles/jsonFileHandler.js
@@ -16,6 +16,15 @@ var connection = mysql.createConnection({
     database: config.sqlDatabase
 });
 
+class patchNoteClass {
+    constructor(buildId, buildComment) {
+        this.buildId = buildId;
+        this.buildComment = buildComment;
+    }
+
+    buildId = 0;
+    buildComment = "";
+}
 
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
@@ -48,29 +57,71 @@ updateLatestSteamBuild = function(eventToEmit, buildValue, buildType) {
 getUpdateLogsFromJenkins = function(eventToEmit, buildValue) {
     console.log('Jenkins Build Value is: '+ buildValue);
 
-    connection.query('select dhBuildVersion, dhStatusReason from deploy_history where dhBuildTarget = "arcanewaters-WindowsServer" and dhBuildVersion > ' + buildValue, function(err,result){
+    connection.query('select dhBuildVersion, dhChangesetId, dhStatusReason from deploy_history where dhBuildTarget = "arcanewaters--ArcaneWaters-Server-Dev-Windows" and dhStatus = "COMPLETED" and dhBuildVersion > ' + buildValue, function(err,result){
         if (err) {
+            console.log('Failed to fetch Jenkins Sql Data Complete');
             console.error(err);
-            getCommentsFromPlastic('finishedCheckingJenkins', result);
+            eventToEmit.emit('finishedCheckingJenkins', result);
         } else {
             console.log('Read Jenkins Sql Data Complete');
-            eventToEmit.emit('finishedCheckingJenkins', result);
+            getCommentsFromPlastic(eventToEmit, result);
         }
     });
+
 }
 
-getCommentsFromPlastic = function(eventToEmit, plasticId) {
-    console.log('Jenkins Build Value is: '+ buildValue);
+getCommentsFromPlastic = function(eventToEmit, cachedContent) {
+    var newClassList = [];
+    var hasCompletedQuery = false;
+    var queryResponseCounte = 0;
+    var buildIdArray = [];
 
-    connection.query('select dhBuildVersion, message from deploy_history where dhBuildTarget = "arcanewaters-WindowsServer" and dhBuildVersion > ' + buildValue, function(err,result){
-        if (err) {
-            console.error(err);
-            eventToEmit.emit('finishedCheckingJenkins', result);
-        } else {
-            console.log('Read Jenkins Sql Data Complete');
-            eventToEmit.emit('finishedCheckingJenkins', result);
+    // Log cache to make sure the process is working properly
+    for (var i = 0 ; i < cachedContent.length ; i++) {
+        console.log("Parameter cache is: "+cachedContent[i].dhBuildVersion);
+        buildIdArray.push(cachedContent[i].dhBuildVersion);
+    }
+    for (var i = 0 ; i < buildIdArray.length ; i++) {
+        console.log("BuildId cache is: "+buildIdArray[i]);
+    }
+
+    if (cachedContent.length < 1) {
+        eventToEmit.emit('finishedCheckingJenkins', newClassList);
+    } else  {
+        for (var i = 0 ; i < cachedContent.length ; i++) {
+            console.log("Fetching from database: "+cachedContent[i].dhBuildVersion + " "+cachedContent[i].dhChangesetId)
+            var queryString = 'SELECT * FROM global.plastic_changeset where id = ' + cachedContent[i].dhChangesetId;
+            connection.query(queryString, function(err,result){
+                if (err) {
+                    console.log('Failed to fetch plasitc changeset');
+                    console.error(err);
+                } else {
+                    console.log('No error in fetching plasitc changeset: '+result.length+" Index: "+i);
+                    for (var q = 0 ; q < result.length ; q++) {
+                        var newClass = new patchNoteClass (
+                            buildIdArray[i],
+                            result[q].comment
+                        );
+                        
+                        newClassList.push(newClass);
+                    }
+                    queryResponseCounte++;
+
+                    if (queryResponseCounte >= cachedContent.length && !hasCompletedQuery) {
+                        hasCompletedQuery = true;
+                        console.log('-------------- Finished fetching from database '+newClassList.length+' --------------');
+
+                        for (var r = 0 ; r < newClassList.length ; r++) {
+                            newClassList[r].buildId = buildIdArray[r];
+                            console.log('-> CompletedQuery: '+newClassList[r].buildId+ " : " +newClassList[r].buildComment);
+
+                        }
+                        eventToEmit.emit('finishedCheckingJenkins', newClassList);
+                    }
+                }
+            });
         }
-    });
+    }
 }
 
 connectionTest = function(eventToEmit) {
