f934fba3616051fc3279b1b931999e757e885e9f
Dev_Burlin <burlin_gauna@yahoo.com>
2021-02-07 17:30:45 +0800

    added condition that prevents users from picking up the ores locally if the voyage id is invalid added visual effect codes upon collding with the pickup ore disabled ore pickup object to prevent multiple pickup triggers added warning messages

--- Arcane Waters - Alpha/Assets/Scripts/Mining/OrePickup.cs
+++ Arcane Waters - Alpha/Assets/Scripts/Mining/OrePickup.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using Mirror;
+using TMPro;
 
 public class OrePickup : MonoBehaviour {
    #region Public Variables
@@ -35,8 +36,31 @@ public class OrePickup : MonoBehaviour {
 
    private void OnTriggerEnter2D (Collider2D collision) {
       if (collision.GetComponent<PlayerBodyEntity>() != null && collision.GetComponent<PlayerBodyEntity>() == Global.player) {
+         if (voyageGroupId < 1) {
+            triggerWarningMessage("Invalid Voyage Group!");
+            return;
+         }
+         if (collision.GetComponent<PlayerBodyEntity>().voyageGroupId != voyageGroupId) {
+            triggerWarningMessage("This does not belong to you!");
+            return;
+         }
+
+         // TODO: Create sprite effects for ore collect
+         EffectManager.self.create(Effect.Type.Crop_Harvest, transform.position);
+         EffectManager.self.create(Effect.Type.Crop_Shine, transform.position);
+         EffectManager.self.create(Effect.Type.Crop_Dirt_Large, transform.position);
+
          Global.player.rpc.Cmd_MineNode(oreNode.id, oreEffectId, ownerId, voyageGroupId);
+         GetComponent<BoxCollider2D>().enabled = false;
+         gameObject.SetActive(false);
       }
+   }
+
+   private void triggerWarningMessage (string msg) {
+      Vector3 pos = this.transform.position + new Vector3(0f, .32f);
+      GameObject messageCanvas = Instantiate(PrefabsManager.self.warningTextPrefab);
+      messageCanvas.transform.position = pos;
+      messageCanvas.GetComponentInChildren<TextMeshProUGUI>().text = msg;
    }
 
    #region Private Variables
