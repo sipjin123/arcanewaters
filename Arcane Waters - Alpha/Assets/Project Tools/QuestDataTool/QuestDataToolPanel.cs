9987306b3357ab8fea11d56318e941ebc433c828
Dev_Burlin <burlin_gauna@yahoo.com>
2021-02-16 13:44:19 +0800

    removed obsolete cloud build data checker

--- Arcane Waters - Alpha/Assets/Project Tools/QuestDataTool/QuestDataToolPanel.cs
+++ Arcane Waters - Alpha/Assets/Project Tools/QuestDataTool/QuestDataToolPanel.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using Mirror;
-using CloudBuildDataFetch;
 
 public class QuestDataToolPanel : MonoBehaviour {
    #region Public Variables
