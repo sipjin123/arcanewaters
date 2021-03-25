82c560ea407c1e4f78f610711ce2f691b4a62228
Dev_Burlin <burlin_gauna@yahoo.com>
2021-03-25 08:36:13 +0800

    setup key util scripts that handles generic keyboard press and mouse input

---
+++ Arcane Waters - Alpha/Assets/Scripts/Util/KeyUtils.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public static class KeyUtils {
+   // Keyboard keys
+   public static bool isKeyPressed (Key key) {
+      return Keyboard.current[key].isPressed;
+   }
+
+   public static bool isKeyPressedDown (Key key) {
+      return Keyboard.current[key].wasPressedThisFrame;
+   }
+
+   public static bool isKeyPressedUp (Key key) {
+      return Keyboard.current[key].wasReleasedThisFrame;
+   }
+
+   // Left mouse button
+   public static bool isLeftButtonPressed () {
+      return Mouse.current.leftButton.isPressed;
+   }
+
+   public static bool isLeftButtonPressedDown () {
+      return Mouse.current.leftButton.wasPressedThisFrame;
+   }
+
+   public static bool isLeftButtonPressedUp () {
+      return Mouse.current.leftButton.wasReleasedThisFrame;
+   }
+
+   // Right mouse button
+   public static bool isRightButtonPressed () {
+      return Mouse.current.rightButton.isPressed;
+   }
+
+   public static bool isRightButtonPressedDown () {
+      return Mouse.current.rightButton.wasPressedThisFrame;
+   }
+
+   public static bool isRightButtonPressedUp () {
+      return Mouse.current.rightButton.wasReleasedThisFrame;
+   }
+
+   // Mouse Utilities
+   public static Vector2 getMousePosition () {
+      return Mouse.current.position.ReadValue();
+   }
+}
