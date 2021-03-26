f94770ec24b108ea83e8b9fa2a4d4ea83aa4407e
Dev_Burlin <burlin_gauna@yahoo.com>
2021-03-26 17:09:38 +0800

    updated key and mouse utils to return default value if is running on server cloud build to fix null reference of server when accessing input system (TODO: Cleanup script after confirmation of fix)

--- Arcane Waters - Alpha/Assets/Scripts/Util/KeyUtils.cs
+++ Arcane Waters - Alpha/Assets/Scripts/Util/KeyUtils.cs
@@ -1,56 +1,96 @@
-﻿using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using UnityEngine.InputSystem;
 
+// TODO: Cleanup this script after confirming cloud server fix
 public static class KeyUtils {
 
    #region Keyboard keys
 
    public static bool GetKey (Key key) {
+      if (Util.isBatch()) {
+         return false;
+      }
+
+      #if !(IS_SERVER_BUILD && CLOUD_BUILD)
       return Keyboard.current[key].isPressed;
+      #else
+      return false;
+      #endif
    }
 
    public static bool GetKeyDown (Key key) {
+      if (Util.isBatch()) {
+         return false;
+      }
+
+      #if !(IS_SERVER_BUILD && CLOUD_BUILD)
       return Keyboard.current[key].wasPressedThisFrame;
+      #else
+      return false;
+      #endif
    }
 
    public static bool GetKeyUp (Key key) {
+      if (Util.isBatch()) {
+         return false;
+      }
+
+      #if !(IS_SERVER_BUILD && CLOUD_BUILD)
       return Keyboard.current[key].wasReleasedThisFrame;
+      #else
+      return false;
+      #endif
    }
 
    #endregion
 
-
    #region Mouse keys
 
    public static bool GetButton (MouseButton mouseButtonKey) {
+      if (Util.isBatch()) {
+         return false;
+      }
+
+      #if !(IS_SERVER_BUILD && CLOUD_BUILD)
       switch (mouseButtonKey) {
          case MouseButton.Left:
             return Mouse.current.leftButton.isPressed;
          case MouseButton.Right:
             return Mouse.current.rightButton.isPressed;
       }
-
+      #endif
       return false;
    }
 
    public static bool GetButtonDown (MouseButton mouseButtonKey) {
+      if (Util.isBatch()) {
+         return false;
+      }
+
+      #if !(IS_SERVER_BUILD && CLOUD_BUILD)
       switch (mouseButtonKey) {
          case MouseButton.Left:
             return Mouse.current.leftButton.wasPressedThisFrame;
          case MouseButton.Right:
             return Mouse.current.rightButton.wasPressedThisFrame;
       }
+      #endif
 
       return false;
    }
 
    public static bool GetButtonUp (MouseButton mouseButtonKey) {
+      if (Util.isBatch()) {
+         return false;
+      }
+
+      #if !(IS_SERVER_BUILD && CLOUD_BUILD)
       switch (mouseButtonKey) {
          case MouseButton.Left:
             return Mouse.current.leftButton.wasReleasedThisFrame;
          case MouseButton.Right:
             return Mouse.current.rightButton.wasReleasedThisFrame;
       }
+      #endif
 
       return false;
    }
--- Arcane Waters - Alpha/Assets/Scripts/Util/MouseUtils.cs
+++ Arcane Waters - Alpha/Assets/Scripts/Util/MouseUtils.cs
@@ -4,7 +4,14 @@ using UnityEngine.InputSystem;
 public static class MouseUtils {
    public static Vector2 mousePosition  {
       get {
+         if (Util.isBatch()) {
+            return new Vector2(0, 0);
+         }
+         #if !(IS_SERVER_BUILD && CLOUD_BUILD)
          return Mouse.current.position.ReadValue();
+         #else
+         return new Vector2(0, 0);
+         #endif
       }
    }
 }
