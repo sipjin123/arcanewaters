7b7927288af19d7e831f7a31e3a8b6342f1d2058
Dev_Burlin <burlin_gauna@yahoo.com>
2021-04-02 21:02:05 +0800

    setup new game manager base class

---
+++ Arcane Waters - Alpha/Assets/Scripts/Logger/GenericGameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+
+public class GenericGameManager : MonoBehaviour {
+   #region Public Variables
+
+   #endregion
+
+   protected virtual void Awake () {
+      D.debug(GetType().ToString() + ".Awake");
+   }
+
+   #region Private Variables
+      
+   #endregion
+}
