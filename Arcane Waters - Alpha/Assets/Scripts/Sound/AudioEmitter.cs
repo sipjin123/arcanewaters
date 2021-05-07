c725a90f326af3ad779d84abcf4d9f2670df5693
Dev_Burlin <burlin_gauna@yahoo.com>
2021-05-08 04:46:34 +0800

    setup initial audio related prefabs to the map editor (this was before the email discussing about the possibility of skipping this new prefab feature)

---
+++ Arcane Waters - Alpha/Assets/Scripts/Sound/AudioEmitter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+using MapCreationTool;
+using MapCreationTool.Serialization;
+
+public class AudioEmitter : MapEditorPrefab, IPrefabDataListener, IHighlightable {
+   #region Public Variables
+
+   // Components
+   private SpriteRenderer ren;
+   private SpriteOutline outline;
+
+   #endregion
+
+   private void Awake () {
+      ren = GetComponentInChildren<SpriteRenderer>();
+      outline = GetComponentInChildren<SpriteOutline>();
+   }
+
+   public void dataFieldChanged (DataField field) {
+      throw new System.NotImplementedException();
+   }
+
+   public void setHighlight (bool hovered, bool selected, bool deleting) {
+      setOutlineHighlight(outline, hovered, selected, deleting);
+   }
+
+   #region Private Variables
+
+   #endregion
+}
