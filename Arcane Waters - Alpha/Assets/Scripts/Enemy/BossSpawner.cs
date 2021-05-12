45c88de15bbd4657e90d66cb18dcb037564e3dce
Dev_Burlin <burlin_gauna@yahoo.com>
2021-05-13 01:07:48 +0800

    setup boss spawner using map editor tool

---
+++ Arcane Waters - Alpha/Assets/Scripts/Enemy/BossSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+using MapCreationTool.Serialization;
+
+public class BossSpawner : MonoBehaviour, IMapEditorDataReceiver
+{
+   #region Public Variables
+
+   #endregion
+
+   public void receiveData (DataField[] dataFields) {
+
+   }
+
+   #region Private Variables
+
+   #endregion
+}
