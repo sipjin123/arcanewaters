be501bfb9ecc4668f4fa99282fdc70c516517b5d
Dev_Burlin <burlin_gauna@yahoo.com>
2021-06-03 16:42:11 +0800

    updated scroll rect override to  reference the parent object containing all the canvas and compile all the scroll rects in their children

--- Arcane Waters - Alpha/Assets/Scripts/GUI/ScrollRectOverride.cs
+++ Arcane Waters - Alpha/Assets/Scripts/GUI/ScrollRectOverride.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System.Linq;
 
 public class ScrollRectOverride : MonoBehaviour {
 
@@ -12,10 +13,25 @@ public class ScrollRectOverride : MonoBehaviour {
    // List of scroll rect in the game
    public List<ScrollRect> scrollList = new List<ScrollRect>();
 
+   // The parent object containing all of the canvas of the game
+   public GameObject canvasParent;
+
    #endregion 
 
    private void Start () {
       if (Util.isLinux()) {
+         scrollList = new List<ScrollRect>();
+
+         // Get the list of canvas children of the canvas holder
+         List<Canvas> canvasList = canvasParent.GetComponentsInChildren<Canvas>(true).ToList();
+         foreach (Canvas canvasRef in canvasList) {
+            // The list of scroll rect within this canvas
+            List<ScrollRect> scrollListInCanvas = canvasRef.GetComponentsInChildren<ScrollRect>(true).ToList();
+            foreach (ScrollRect scrollRect in scrollListInCanvas) {
+               scrollList.Add(scrollRect);
+            }
+         }
+
          foreach (ScrollRect scroller in scrollList) {
             scroller.scrollSensitivity *= -1;
          }
