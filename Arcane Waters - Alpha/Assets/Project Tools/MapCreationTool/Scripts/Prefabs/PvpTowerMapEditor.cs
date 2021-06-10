8caa332b5201f8810c4c68995c32e4abb5341ac5
Dev_Burlin <burlin_gauna@yahoo.com>
2021-06-11 05:28:49 +0800

    added pvp tower as part of the map editor place able prefab options

---
+++ Arcane Waters - Alpha/Assets/Project Tools/MapCreationTool/Scripts/Prefabs/PvpTowerMapEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+
+public class PvpTowerMapEditor : MonoBehaviour {
+   #region Public Variables
+      
+   #endregion
+
+   #region Private Variables
+      
+   #endregion
+}
