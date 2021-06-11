4277ef2a70000b690c0af69926eb75639cf606e6
Dev_Burlin <burlin_gauna@yahoo.com>
2021-06-12 02:32:14 +0800

    deleted voyage group indicator which is no longer needed / updated mini map ship icon to no longer display arrow indicators

--- Arcane Waters - Alpha/Assets/Scripts/Minimap/MM_ShipEntityIcon.cs
+++ Arcane Waters - Alpha/Assets/Scripts/Minimap/MM_ShipEntityIcon.cs
@@ -13,21 +13,11 @@ public class MM_ShipEntityIcon : MonoBehaviour {
    // Area in which ship should be (otherwise hide)
    public Area currentArea;
 
-   // The icon that will be displayed when this ship icon is out of mini map bounds
-   public RectTransform outBoundIcon;
-
-   // Prefab to spawn when this icon is out of bounds
-   public GameObject outBoundIconPrefab;
-
-   // The current rect transform
-   public RectTransform currentRectTransform;
-
    #endregion
 
    protected void Start () {
       // Lookup components
       _image = GetComponent<Image>();
-      currentRectTransform = GetComponent<RectTransform>();
    }
 
    private void Update () {
@@ -39,41 +29,6 @@ public class MM_ShipEntityIcon : MonoBehaviour {
          Destroy(this.gameObject);
          return;
       }
-
-      if (currentRectTransform == null) {
-         return;
-      } 
-
-      // Set correct ship entity icon position in minimap
-      currentRectTransform.anchoredPosition = Minimap.self.getCorrectedPosition(shipEntity.transform, currentArea);
-
-      // If the icon is out of bounds, generate a sub icon pointing towards this icon
-      if (Mathf.Abs(currentRectTransform.anchoredPosition.x) > Minimap.MAP_CLAMP_VAL || Mathf.Abs(currentRectTransform.anchoredPosition.y) > Minimap.MAP_CLAMP_VAL) {
-         if (outBoundIcon == null) {
-            outBoundIcon = Instantiate(outBoundIconPrefab, transform.parent).GetComponent<RectTransform>();
-
-            if (Global.player != null) {
-               if (Global.player.voyageGroupId == shipEntity.voyageGroupId) {
-                  outBoundIcon.GetComponentInChildren<Image>().color = Color.green;
-               } else {
-                  outBoundIcon.GetComponentInChildren<Image>().color = Color.red;
-               }
-            }
-         }  
-         outBoundIcon.gameObject.SetActive(true);
-
-         float horizontalClamp = Mathf.Clamp(currentRectTransform.anchoredPosition.x, -Minimap.MAP_CLAMP_VAL, Minimap.MAP_CLAMP_VAL);
-         float verticalClamp = Mathf.Clamp(currentRectTransform.anchoredPosition.y, -Minimap.MAP_CLAMP_VAL, Minimap.MAP_CLAMP_VAL);
-         Vector2 clampedPosition = new Vector2(horizontalClamp, verticalClamp);
-         outBoundIcon.anchoredPosition = clampedPosition;
-
-         Vector3 dir = outBoundIcon.anchoredPosition - Minimap.self.playerIcon.GetComponent<RectTransform>().anchoredPosition;
-         outBoundIcon.rotation = Quaternion.Slerp(outBoundIcon.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 100);
-      } else {
-         if (outBoundIcon != null) {
-            Destroy(outBoundIcon.gameObject);
-         }
-      }
    }
 
    public void onHoverBegin () {
@@ -84,10 +39,6 @@ public class MM_ShipEntityIcon : MonoBehaviour {
 
    public void onHoverEnd () {
       Minimap.self.disableIconInfo();
-   }
-
-   private void OnDestroy () {
-      Destroy(outBoundIcon.gameObject);
    }
 
    #region Private Variables
