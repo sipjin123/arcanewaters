aa7b9fdfbf690bec7b9356bba1b823de5dbf601a
Dev_Burlin <burlin_gauna@yahoo.com>
2021-06-15 13:12:35 +0800

    updated chat line to be interactable and will call context menu and to have visual feed on which chat line is hovered

--- Arcane Waters - Alpha/Assets/Scripts/GUI/Chat/SpeakChatLine.cs
+++ Arcane Waters - Alpha/Assets/Scripts/GUI/Chat/SpeakChatLine.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using Mirror;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class SpeakChatLine : ChatLine {
    #region Public Variables
@@ -12,6 +13,24 @@ public class SpeakChatLine : ChatLine {
    public TextMeshProUGUI text;
 
    #endregion
+
+   public override void OnPointerClick (PointerEventData eventData) {
+      if (Global.player != null && Global.player.userId != chatInfo.senderId) {
+         PanelManager.self.contextMenuPanel.showDefaultMenuForUser(chatInfo.senderId, chatInfo.sender);
+      }
+   }
+
+   public void chatLineHoverEnter () {
+      if (Global.player != null && Global.player.userId != chatInfo.senderId) {
+         text.alpha = .75f;
+      }
+   }
+
+   public void chatLineHoverExit () {
+      if (Global.player != null && Global.player.userId != chatInfo.senderId) {
+         text.alpha = 1f;
+      }
+   }
 
    #region Private Variables
 
