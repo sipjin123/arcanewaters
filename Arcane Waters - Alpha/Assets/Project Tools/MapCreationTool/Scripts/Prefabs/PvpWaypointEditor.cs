c4f77ba842c70c29332c096f80b542bca10914b5
Dev_Burlin <burlin_gauna@yahoo.com>
2021-06-16 15:50:22 +0800

    updated map editor to support new pvp prefab which is the waypoint prefab, setup waypoint prefab to accept lane type paraemters

---
+++ Arcane Waters - Alpha/Assets/Project Tools/MapCreationTool/Scripts/Prefabs/PvpWaypointEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+using MapCreationTool;
+using MapCreationTool.Serialization;
+using System;
+
+public class PvpWaypointEditor : MapEditorPrefab, IPrefabDataListener, IHighlightable
+{
+   #region Public Variables
+
+   // The pvp lane
+   public Text pvpLane;
+
+   #endregion
+
+   #region Private Variables
+
+   #endregion
+   public void dataFieldChanged (DataField field) {
+      if (field.k.CompareTo(DataField.PVP_LANE) == 0) {
+         try {
+            PvpLane pvpLaneVal = (PvpLane) Enum.Parse(typeof(PvpLane), field.v);
+            pvpLane.text = pvpLaneVal.ToString();
+         } catch {
+            pvpLane.text = PvpLane.None.ToString();
+         }
+      }
+   }
+
+   public void setHighlight (bool hovered, bool selected, bool deleting) {
+   }
+}
