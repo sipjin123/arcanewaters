045c7657632d7eb90912c004af7a976cfab6abc4
Dev_Burlin <burlin_gauna@yahoo.com>
2021-06-18 18:35:26 +0800

    pvp waypoint now fetches lane data to be used by ship ai

--- Arcane Waters - Alpha/Assets/Scripts/Sea/Sea Structures/PvpWaypoint.cs
+++ Arcane Waters - Alpha/Assets/Scripts/Sea/Sea Structures/PvpWaypoint.cs
@@ -3,13 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using Mirror;
+using MapCreationTool.Serialization;
+using System;
 
-public class PvpWaypoint : MonoBehaviour {
+public class PvpWaypoint : MonoBehaviour, IMapEditorDataReceiver {
    #region Public Variables
-      
+
+   public PvpLane lane = PvpLane.None;
+
+   public void receiveData (DataField[] dataFields) {
+      foreach (DataField field in dataFields) {
+         if (field.k.CompareTo(DataField.PVP_LANE) == 0) {
+            try {
+               PvpLane pvpLane = (PvpLane) Enum.Parse(typeof(PvpLane), field.v);
+               this.lane = pvpLane;
+            } catch {
+               this.lane = PvpLane.None;
+            }
+         }
+      }
+   }
+
    #endregion
 
    #region Private Variables
-      
+
    #endregion
 }
