56f57f63bc6e3cab657bf2963c113b064e8d33bf
Dev_Burlin <burlin_gauna@yahoo.com>
2021-06-18 18:29:18 +0800

    updated map tool to support new pvp prefab which is the monster spawner

---
+++ Arcane Waters - Alpha/Assets/Project Tools/MapCreationTool/Scripts/Prefabs/PvpMonsterSpawnerMapEditor.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+using MapCreationTool;
+using MapCreationTool.Serialization;
+using System;
+using System.Linq;
+
+namespace MapCreationTool {
+   public class PvpMonsterSpawnerMapEditor : MapEditorPrefab, IPrefabDataListener, IHighlightable {
+
+      #region Public Variables
+
+      // Displays the monster assigned to this spawner
+      public SpriteRenderer monsterDisplay;
+
+      // Displays the powerup assigned to this spawner
+      public SpriteRenderer powerupDisplay;
+
+      // The name of the powerup
+      public Text powerupName;
+
+      #endregion
+
+      private void Awake () {
+         _outline = GetComponentInChildren<SpriteOutline>();
+      }
+
+      public void dataFieldChanged (DataField field) {
+         if (field.k.CompareTo(DataField.SEA_ENEMY_DATA_KEY) == 0) {
+            try {
+               SeaMonsterEntity.Type seaMonsterType = (SeaMonsterEntity.Type) Enum.Parse(typeof(SeaMonsterEntity.Type), field.v);
+               SeaMonsterXMLContent monsterData = MonsterManager.instance.getSeaMonsters().Find(_ => _.seaMonsterData.seaMonsterType == seaMonsterType);
+               monsterDisplay.sprite = ImageManager.getSprite(monsterData.seaMonsterData.avatarSpritePath);
+            } catch {
+
+            }
+         }
+
+         if (field.k.CompareTo(DataField.PVP_MONSTER_POWERUP) == 0) {
+            try {
+               Powerup.Type powerupType = (Powerup.Type) Enum.Parse(typeof(Powerup.Type), field.v);
+               Sprite[] iconSprites = Resources.LoadAll<Sprite>(Powerup.ICON_SPRITES_LOCATION);
+               powerupDisplay.sprite = iconSprites[(int) powerupType - 1];
+               powerupName.text = powerupType.ToString();
+            } catch {
+
+            }
+         }
+      }
+
+      public void setHighlight (bool hovered, bool selected, bool deleting) {
+         if (_outline != null) {
+            setOutlineHighlight(_outline, hovered, selected, deleting);
+         }
+      }
+
+      #region Private Variables
+
+      // The outline
+      private SpriteOutline _outline;
+
+      #endregion
+   }
+}
---
+++ Arcane Waters - Alpha/Assets/Scripts/Sea/Sea Structures/PvpMonsterSpawner.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+using MapCreationTool.Serialization;
+using System;
+
+public class PvpMonsterSpawner : MonoBehaviour, IMapEditorDataReceiver {
+   #region Public Variables
+
+   // The type of seamonster this spawner will generate
+   public SeaMonsterEntity.Type seaMonsterType = SeaMonsterEntity.Type.None;
+
+   // The type of powerup this spawner will generate
+   public Powerup.Type powerupType;
+
+   // The instance id
+   public int instanceId;
+
+   // The delay before the spawning starts
+   public const int START_DELAY = 10;
+
+   // The delay before the the monster will respawn
+   public const int RESPAWN_DELAY = 30;
+
+   #endregion
+
+   private void Start () {
+      Invoke(nameof(spawnMonster), START_DELAY);
+   }
+
+   private void Update () {
+      if (!NetworkServer.active) {
+         return;
+      }
+
+      if (KeyUtils.GetKeyDown(UnityEngine.InputSystem.Key.X)) {
+         spawnMonster();
+      }
+   }
+
+   public void initializeSpawner () {
+      // TODO: This function should be called by the pvp game when the match officially starts
+      spawnMonster();
+   }
+
+   private void spawnMonster () {
+      SeaMonsterEntity seaEntity = Instantiate(PrefabsManager.self.seaMonsterPrefab);
+      SeaMonsterEntityData data = SeaMonsterManager.self.getMonster(seaMonsterType);
+      Instance instance = InstanceManager.self.getInstance(instanceId);
+
+      // Basic setup
+      seaEntity.monsterType = data.seaMonsterType;
+      seaEntity.areaKey = instance.areaKey;
+      seaEntity.facing = Direction.South;
+      seaEntity.isPvpMonster = true;
+
+      // Transform setup
+      seaEntity.transform.SetParent(transform, false);
+      seaEntity.transform.localPosition = Vector3.zero;
+
+      // Network Setup
+      InstanceManager.self.addSeaMonsterToInstance(seaEntity, instance);
+      NetworkServer.Spawn(seaEntity.gameObject);
+
+      seaEntity.hasDiedEvent.AddListener(() => {
+         uint lastAttackerId = seaEntity.lastAttackerId();
+         NetEntity lastAttacker = MyNetworkManager.fetchEntityFromNetId<NetEntity>(lastAttackerId);
+         if (lastAttacker != null) {
+            if (lastAttacker.tryGetGroup(out VoyageGroupInfo voyageGroup)) {
+               // Send the result to all group members
+               foreach (int userId in voyageGroup.members) {
+                  NetEntity memberEntity = EntityManager.self.getEntity(userId);
+                  if (memberEntity != null && memberEntity is PlayerShipEntity) {
+                     memberEntity.rpc.Target_ReceivePowerup(powerupType);
+                  }
+               }
+            }
+         }
+         Invoke(nameof(spawnMonster), RESPAWN_DELAY);
+         seaEntity.hasDiedEvent.RemoveAllListeners();
+      });
+   }
+
+   public void receiveData (DataField[] dataFields) {
+      foreach (DataField field in dataFields) {
+         if (field.k.CompareTo(DataField.SEA_ENEMY_DATA_KEY) == 0) {
+            try {
+               SeaMonsterEntity.Type seaMonsterType = (SeaMonsterEntity.Type) Enum.Parse(typeof(SeaMonsterEntity.Type), field.v);
+               this.seaMonsterType = seaMonsterType;
+            } catch {
+
+            }
+         }
+
+         if (field.k.CompareTo(DataField.PVP_MONSTER_POWERUP) == 0) {
+            try {
+               Powerup.Type powerupType = (Powerup.Type) Enum.Parse(typeof(Powerup.Type), field.v);
+               this.powerupType = powerupType;
+            } catch {
+
+            }
+         }
+      }
+   }
+
+   #region Private Variables
+
+   #endregion
+}
