9900d231f4de51ba1bf255c430276f37ca1af295
Dev_Burlin <burlin_gauna@yahoo.com>
2021-06-21 18:29:56 +0800

    setup pvp stat panel and how data is populated per player stat bar

---
+++ Arcane Waters - Alpha/Assets/Scripts/GUI/Pvp/PvpStatPanel.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+
+public class PvpStatPanel : Panel {
+   #region Public Variables
+
+   // The object holding all pvp stat rows
+   public Transform pvpStatRowHolder;
+
+   // Prefab of the pvp stat row
+   public PvpStatRow pvpStatRowPrefab;
+
+   // Self
+   public static PvpStatPanel self;
+
+   #endregion
+
+   public override void Awake () {
+      base.Awake();
+      self = this;
+   }
+
+   public void populatePvpPanelData (PvpStatsData pvpStatData) {
+      pvpStatRowHolder.gameObject.DestroyChildren();
+
+      foreach (PvpPlayerStat playerStat in pvpStatData.playerStats) {
+         PvpStatRow statRow = Instantiate(pvpStatRowPrefab, pvpStatRowHolder);
+         statRow.kills.text = playerStat.playerKills.ToString();
+         statRow.assists.text = playerStat.playerAssists.ToString();
+         statRow.deaths.text = playerStat.playerDeaths.ToString();
+         statRow.monsterKills.text = playerStat.playerMonsterKills.ToString();
+         statRow.buildingsDestroyed.text = playerStat.playerStructuresDestroyed.ToString();
+         statRow.shipKills.text = playerStat.playerShipKills.ToString();
+
+         statRow.userName.text = playerStat.playerName.ToString();
+      }
+   }
+
+   #region Private Variables
+
+   #endregion
+}
---
+++ Arcane Waters - Alpha/Assets/Scripts/GUI/Pvp/PvpStatRow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+
+public class PvpStatRow : MonoBehaviour {
+   #region Public Variables
+
+   // The text fields representing each stat
+   public Text kills, deaths, shipKills, monsterKills, assists, userName, buildingsDestroyed;
+
+   #endregion
+
+   #region Private Variables
+      
+   #endregion
+}
