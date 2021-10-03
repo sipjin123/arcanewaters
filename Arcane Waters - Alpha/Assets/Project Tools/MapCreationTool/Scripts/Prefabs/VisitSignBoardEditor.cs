3360179d86d5d356598ad74df88c5af1bcd0617d
Dev_Burlin <burlin_gauna@yahoo.com>
2021-10-03 09:31:59 +0800

    setup visit sign to the map editor and in game spawn which is located at the north of the tutorial town

---
+++ Arcane Waters - Alpha/Assets/Project Tools/MapCreationTool/Scripts/Prefabs/VisitSignBoardEditor.cs
@@ -0,0 +1,18 @@
+﻿using MapCreationTool;
+using MapCreationTool.Serialization;
+
+public class VisitSignBoardEditor : MapEditorPrefab, IPrefabDataListener, IHighlightable {
+   #region Public Variables
+
+   #endregion
+
+   public void dataFieldChanged (DataField field) {
+   }
+
+   public void setHighlight (bool hovered, bool selected, bool deleting) {
+   }
+
+   #region Private Variables
+
+   #endregion
+}
---
+++ Arcane Waters - Alpha/Assets/Scripts/Visit/VisitSignBoard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+
+public class VisitSignBoard : MonoBehaviour {
+   #region Public Variables
+
+   #endregion
+
+   public void triggerVisitPanel () {
+      BottomBar.self.toggleFriendVisitPanel();
+   }
+
+   #region Private Variables
+      
+   #endregion
+}
