30fa0a3fe45a647288d23b3c729add627d2fa962
Dev_Burlin <burlin_gauna@yahoo.com>
2021-10-10 02:21:39 +0800

    visual updates for ability orb / updated visit row button functionality reference / added constant for ability status / removed duplicate script in master tool scene

--- Arcane Waters - Alpha/Assets/Project Tools/ShipAbilityTool/ShipAbilityData.cs
+++ Arcane Waters - Alpha/Assets/Project Tools/ShipAbilityTool/ShipAbilityData.cs
@@ -102,6 +102,9 @@ public class ShipAbilityData
    // If the buff effect has a radius of effectivity
    public float buffRadius = 0;
 
+   // The cast delay of status abilities
+   public const float STATUS_CAST_DELAY = 1;
+
    // If the ability will only take effect within buff radius
    // Some abilities will take effect when (casted only) within radius while some will only take effect while (staying) in radius
    public bool isBuffRadiusDependent = false;
