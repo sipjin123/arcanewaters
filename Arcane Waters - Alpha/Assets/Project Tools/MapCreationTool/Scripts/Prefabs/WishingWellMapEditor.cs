1f2772c27797af84240066614c3549e308502a80
Dev_Burlin <burlin_gauna@yahoo.com>
2021-10-21 09:47:12 +0800

    setup wishing well in map editor and ingame flow

---
+++ Arcane Waters - Alpha/Assets/Project Tools/MapCreationTool/Scripts/Prefabs/WishingWellMapEditor.cs
@@ -0,0 +1,31 @@
+﻿using MapCreationTool;
+using MapCreationTool.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MapCreationTool {
+   public class WishingWellMapEditor : MapEditorPrefab, IPrefabDataListener, IHighlightable {
+      #region Public Variables
+
+      // The object holding the range display
+      public GameObject rangeDisplay;
+
+      #endregion
+
+      private void Start () {
+         if (transform.GetComponentInParent<Palette>() != null) {
+            rangeDisplay.SetActive(false);
+         }
+      }
+
+      public void dataFieldChanged (DataField field) {
+      }
+
+      public void setHighlight (bool hovered, bool selected, bool deleting) {
+      }
+
+      #region Private Variables
+
+      #endregion
+   }
+}
---
+++ Arcane Waters - Alpha/Assets/Scripts/LandStructures/WishingWell.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Mirror;
+using MapCreationTool.Serialization;
+
+public class WishingWell : MonoBehaviour, IMapEditorDataReceiver
+{
+   #region Public Variables
+
+   // The shop id reference
+   public int pvpShopId;
+
+   // If within trigger distance
+   public bool isWithinDistance;
+
+   #endregion
+
+   private void Awake () {
+      _outline = GetComponentInChildren<SpriteOutline>();
+   }
+
+   public void clientClickedMe () {
+      if (isWithinDistance) {
+         PvpShopPanel.self.shopId = pvpShopId;
+         PvpShopPanel.self.onShopButtonPressed(false);
+      }
+   }
+
+   private void OnTriggerEnter2D (Collider2D collision) {
+      PlayerBodyEntity playerBody = collision.GetComponent<PlayerBodyEntity>();
+      if (Global.player != null && playerBody != null && Global.player.userId == playerBody.userId) {
+         isWithinDistance = true;
+      }
+   }
+
+   private void OnTriggerExit2D (Collider2D collision) {
+      PlayerBodyEntity playerBody = collision.GetComponent<PlayerBodyEntity>();
+      if (Global.player != null && playerBody != null && Global.player.userId == playerBody.userId) {
+         isWithinDistance = false;
+      }
+   }
+
+   public void onHoverEnter () {
+      if (_outline == null) {
+         return;
+      }
+
+      _outline.setVisibility(true);
+   }
+
+   public void onHoverExit () {
+      if (_outline == null) {
+         return;
+      }
+
+      _outline.setVisibility(false);
+   }
+
+   public void receiveData (DataField[] dataFields) {
+      foreach (DataField dataField in dataFields) {
+         if (dataField.k.CompareTo(DataField.SHOP_ID) == 0) {
+            try {
+               int intVal = int.Parse(dataField.v.Split(':')[0]);
+               pvpShopId = intVal;
+            } catch {
+            }
+         }
+      }
+   }
+
+   #region Private Variables
+
+   // Our various components
+   protected SpriteOutline _outline;
+
+   #endregion
+}
