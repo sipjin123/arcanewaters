9563a3648ceea6f2ce56d1d232b539d71728a63a
Dev_Burlin <burlin_gauna@yahoo.com>
2021-11-09 22:26:01 +0800

    updated ore node controller to override ores to be inactive if the count exceeds the declared count / setup respawn timer to be set for ore nodes

--- Arcane Waters - Alpha/Assets/Scripts/Mining/OreNodeMapController.cs
+++ Arcane Waters - Alpha/Assets/Scripts/Mining/OreNodeMapController.cs
@@ -10,7 +10,7 @@ public class OreNodeMapController : MonoBehaviour, IMapEditorDataReceiver
    #region Public Variables
 
    // The respawn timer
-   public float oreRespawnTimer = 9999;
+   public float oreRespawnTimer = 888;
 
    // The total allowed active ores
    public int oreMaxActiveCount = 1;
@@ -18,10 +18,40 @@ public class OreNodeMapController : MonoBehaviour, IMapEditorDataReceiver
    // List of ore nodes
    public List<OreNode> oreSpotList = new List<OreNode>();
 
+   // The ore lifetime before respawning
+   public Dictionary<OreNode, double> oreLifetime = new Dictionary<OreNode, double>();
+
    #endregion
+
+   private IEnumerator CO_Initialize () {
+      yield return new WaitForSeconds(3);
+      oreLifetime = new Dictionary<OreNode, double>();
+
+      foreach (OreNode oreNode in oreSpotList) {
+         double startTime = NetworkTime.time;
+         oreLifetime.Add(oreNode, startTime);
+         oreNode.refreshTimer = oreRespawnTimer;
+         oreNode.resetSettings();
+      }
+
+      if (oreSpotList.Count > oreMaxActiveCount) {
+         List<OreNode> oreNodesToDisable = new List<OreNode>();
+         List<OreNode> oreNodeListCopy = oreSpotList;
+
+         int totalOresToDisable = oreSpotList.Count - oreMaxActiveCount;
+         while (oreNodesToDisable.Count < totalOresToDisable) {
+            OreNode randomOre = oreNodeListCopy.ChooseRandom();
+            if (oreNodesToDisable.Find(_ => _ == randomOre) == null) {
+               randomOre.isDisabledByController = true;
+               oreNodesToDisable.Add(randomOre);
+            }
+         }
+      }
+   }
 
    public void setOreSpotList (List<OreNode> oreSpotList) {
       this.oreSpotList = oreSpotList;
+      StartCoroutine(CO_Initialize());
    }
 
    public void receiveData (DataField[] dataFields) {
